d7df7e76bf00d43d2feb9712c88c7601827f1f76
Wellington-Silva <wellingtonsilva112000@gmail.com>
2022-10-20 08:54:58 -0300

    Fiz bugs

--- src/modules/vars.mjs
+++ src/modules/vars.mjs
@@ -7,5 +7,6 @@ export default {
     port: env.PORT || 3000,
     db_credentials: env.DB_CREDENCIALS,
     secretJwt: env.SECRETJWT,
-    expiresIn: env.EXPIRATION
+    expiresIn: env.EXPIRATION,
+    hash_secret: env.HASH_SECRET
 }
