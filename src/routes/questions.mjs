c40458f71973647329b4586a58caa78cdf6a72cd
Wellington-Silva <wellingtonsilva112000@gmail.com>
2023-01-05 08:36:25 -0300

    Fix bugs

--- src/routes/questions.mjs
+++ src/routes/questions.mjs
@@ -207,62 +207,10 @@ routes.put('/bestanswer/:questionID/:answerIndex', async (req, res) => {
 //     }
 // });
 
-//  Reagir a uma pergunta
-// routes.put('/like/:questionId', async (req, res) => {
-//     const authHeader = req.headers.authentication;
-//     const { _id } = jwt.verify(authHeader);
-//     const { questionId } = req.params;
-//     const { react } = req.body;
-//     const data = {
-//         userId: ObjectId(_id),
-//         react: react,
-//         createdAt: new Date(),
-//         updatedAt: new Date()
-//     }
-//     try {
-//         database.db('QuestionBoxDB').collection('reacts').insertOne(data, (err, result) => {
-//             if (err || !result) return res.status(401).json({ error: true, message: "Não é possível reagir" })
-//             database.db('QuestionBoxDB').collection('questions').updateOne({ _id: ObjectId(questionId) },
-//                 (react === true) ? { $inc: { qtdLikes: +1 } } : { $inc: { qtdNoLikes: +1 } })
-//                 .then((result) => {
-//                     result.modifiedCount
-//                         ? res.status(200).json({ error: false, message: "Reação registrada" })
-//                         : res.status(200).json({ error: true, message: "Não foi possível reagir a essa pergunta" })
-//                 });
-//         });
-//     } catch (e) {
-//         res.status(e?.status || 500).json({ error: true, message: e?.message || "Houve um erro interno no servidor" });
-//     }
-// });
-
-//  Editar uma resposta
-// routes.put('/answer/:questionId/:answerId', async (req, res) => {
-//     const token = req.headers.authentication;
-//     const { _id } = jwt.verify(token);
-//     const { questionId, answerId } = req.params; // Identificador da Pergunta
-//     const { text } = req?.body; // Texto para atualizar
-//     try {
-//         database
-//             .db('QuestionBoxDB')
-//             .collection('questions')
-//             .updateOne(
-//                 { _id: ObjectId(questionId), responses: { $elemMatch: { userId: ObjectId(_id), _id: ObjectId(answerId) } } },
-//                 { $set: { "responses.$.text": text.toString(), "responses.$.updatedAt": new Date() } }
-//             )
-//             .then((resultResponse) => {
-//                 resultResponse.modifiedCount
-//                     ? res.status(200).json({ error: false, message: "A sua resposta foi editada" })
-//                     : res.status(503).json({ error: true, message: "A pergunta que você tentou responder não foi encontrada" })
-//             });
-//     } catch (e) {
-//         res.status(e?.status || 500).json({ error: true, message: "Houve um erro interno " + (e?.message || "") });
-//     }
-// });
-
 // Mostrar uma pergunta específica
 routes.get('/buscar/:id', async (req, res) => {
     const { id } = req.params
-    
+
     try {
         database.db("QuestionBoxDB").collection("questions").findOne({ _id: ObjectId(id) }, (err, result) => {
             console.log(result)
