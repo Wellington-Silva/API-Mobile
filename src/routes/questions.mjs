e542801b2133ea72562e9f8924598c0b7d2ee6ea
Wellington-Silva <wellingtonsilva112000@gmail.com>
2023-01-10 20:30:12 -0300

    Delete Response

--- src/routes/questions.mjs
+++ src/routes/questions.mjs
@@ -184,33 +184,53 @@ routes.put('/bestanswer/:questionID/:answerIndex', async (req, res) => {
 });
 
 //  Editar uma resposta
-// routes.put('/answer/:questionId/:answerId', async (req, res) => {
-//     const token = req.headers.authentication;
-//     const { _id } = jwt.verify(token);
-//     const { questionId, answerId } = req.params; // Identificador da Pergunta
-//     const { text } = req?.body; // Texto para atualizar
-//     try {
-//         database
-//             .db('QuestionBoxDB')
-//             .collection('questions')
-//             .updateOne(
-//                 { _id: ObjectId(questionId), responses: { $elemMatch: { userId: ObjectId(_id), _id: ObjectId(answerId) } } },
-//                 { $set: { "responses.$.text": text.toString(), "responses.$.updatedAt": new Date() } }
-//             )
-//             .then((resultResponse) => {
-//                 resultResponse.modifiedCount
-//                     ? res.status(200).json({ error: false, message: "A sua resposta foi editada" })
-//                     : res.status(503).json({ error: true, message: "A pergunta que você tentou responder não foi encontrada" })
-//             });
-//     } catch (e) {
-//         res.status(e?.status || 500).json({ error: true, message: "Houve um erro interno " + (e?.message || "") });
-//     }
-// });
+routes.put('/answer/:questionId/:answerId', async (req, res) => {
+    const token = req.headers.authentication;
+    const { _id } = jwt.verify(token);
+    const { questionId, answerId } = req.params; // Identificador da Pergunta
+    const { text } = req?.body; // Texto para atualizar
+    try {
+        database
+            .db('QuestionBoxDB')
+            .collection('questions')
+            .updateOne(
+                { _id: ObjectId(questionId), responses: { $elemMatch: { userId: ObjectId(_id), _id: ObjectId(answerId) } } },
+                { $set: { "responses.$.text": text.toString(), "responses.$.updatedAt": new Date() } }
+            )
+            .then((resultResponse) => {
+                resultResponse.modifiedCount
+                    ? res.status(200).json({ error: false, message: "A sua resposta foi editada" })
+                    : res.status(503).json({ error: true, message: "A pergunta que você tentou responder não foi encontrada" })
+            });
+    } catch (e) {
+        res.status(e?.status || 500).json({ error: true, message: "Houve um erro interno " + (e?.message || "") });
+    }
+});
+
+// Apagar uma resposta
+routes.delete('/delete/:id/:indice', async (req, res) => {
+    const token = req.headers.authentication;
+    const { _id } = jwt.verify(token);
+    const { id, indice } = req.params; // ID da pergunta
+    try {
+        database
+            .db('QuestionBoxDB')
+            .collection('questions')
+            // .updateOne({ _id: ObjectId(_id) }, { $pull: { responses: [indice] } })
+            .deleteOne({ _id: ObjectId(id), responses: [indice] }) // Verificar se cria responses como array ou objeto (tem que ser array)
+            .then((result) => {
+                result.acknowledged
+                    ? res.status(200).json(result)
+                    : res.status(403).json({ error: true, message: "Não foi possível excluir essa resposta" })
+            });
+    } catch (e) {
+        res.status(e?.status || 500).json({ error: true, message: "Houve um erro interno " + (e?.message || "") });
+    }
+});
 
 // Mostrar uma pergunta específica
 routes.get('/buscar/:id', async (req, res) => {
     const { id } = req.params
-
     try {
         database.db("QuestionBoxDB").collection("questions").findOne({ _id: ObjectId(id) }, (err, result) => {
             console.log(result)
